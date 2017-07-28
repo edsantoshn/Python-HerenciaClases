212481e345a923404985d823b4206053d1731d28
Edu Santos Paz <eduardo@edsantoshn.com>
2017-07-28 11:35:52 -0600

    Funciones

--- Granja.py
+++ Granja.py
@@ -2,9 +2,17 @@ import Conejos
 #Importamos el modulo Conejos que hereda de la clase Animales
 import Elefante
 
-conejos = Conejos.Conejos(6,14)
-#Instanciamos la clase e inicializamos la clase conejos que hereda
-#la funcion __init__ de la clase Animales
+def crear_conejos(self,cantidad_conejos,cantidad_conejos_parto):
+	return Conejos.Conejos(cantidad_conejos,cantidad_conejos_parto)
+	#Instanciamos la clase e inicializamos la clase conejos que hereda
+	#la funcion __init__ de la clase Animales
+def crear_elefantes(self,cantidad_elefantes,cantidad_elefantes_parto):
+	return Elefante.Elefante(1,16)
+	#Instancia la clase de Elefante y la construye
+
+cantidad_conejos_input = int(input('Ingrese la cantidad de conejos existe: '))
+cantidad_conejos_parto_input = int(input('Ingrese la cantidad de conejos por parto: '))
+conejos = crear_conejos(cantidad_conejos_input,cantidad_conejos_parto_input)
 
 conejos.Repoducir(4)
 conejos.Repoducir(4)
@@ -15,8 +23,10 @@ conejos.Alimentarse("Pasto",100)
 
 conejos.ImprimirPartos()
 
-elefante = Elefante.Elefante(1,16)
-#Instancia la clase de Elefante y la construye
+
+cantidad_elefantes_input = int(input('Ingrese la cantidad de elefantes existe: '))
+cantidad_elefantes_parto_input = int(input('Ingrese la cantidad de elefantes por parto: '))
+elefante = crear_elefantes(cantidad_elefantes_input,cantidad_elefantes_parto_input)
 
 elefante.Repoducir(6)
 elefante.Repoducir(4)
