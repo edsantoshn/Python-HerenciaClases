2992a6ddbbaf2680ca225f595c2430d4f7bbdbd9
Edu Santos Paz <eduardo@edsantoshn.com>
2017-08-01 20:20:06 -0600

    Update

--- Elefante.py
+++ Elefante.py
@@ -1,18 +1,18 @@
 import Animales
 class Elefante(Animales.Animales):
 	"""Importamos el modulo Animales y le heredamos todos sus metodos (funciones) y atributos, construimos metodos especificios para los elefantes"""
-	def TomarAgua(self,tipoagua,cantidadagua):
+	def tomar_agua(self,tipo_agua,cantidad_agua):
 		"""Recibe el tipo de agua y la cantidad de la misma, si el agua es limpia procede a tomar caso contrario manda error"""
-		if(tipoagua.lower()=="limpia"):
-			self.CantidadAgua+=cantidadagua
+		if(tipo_agua.lower()=="limpia"):
+			self.cantidad_agua+=cantidad_agua
 		else:
 			print("El agua no es adecuada, busquemos otro lugar")
 
-	def Comer(self,tipoalimento,cantidadcomer):
+	def Comer(self,tipo_alimento,cantidad_comer):
 		"""Recibe el tipo de alimento y la cantidad del mismo, si el alimento es mani el elefante procede a comer, caso contrario manda error"""
-		if(tipoalimento.lower()=="mani"):
-			self.CantidadAlimento+=cantidadcomer
-			self.TipoAlimento=tipoalimento
+		if(tipo_alimento.lower()=="mani"):
+			self.cantidad_alimento+=cantidad_comer
+			self.tipo_alimento=tipo_alimento
 		else:
 			print("Solo me alimento de manies")
 			
