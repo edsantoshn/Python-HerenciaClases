9e270372b03ea62dae3a44c294825a304b8e8822
Edu Santos Paz <eduardo@edsantoshn.com>
2017-08-01 20:18:10 -0600

    Update

--- Conejos.py
+++ Conejos.py
@@ -2,12 +2,12 @@ import Animales
 #Importamos el Modulo Animales para utilizar como clase padre la clase Animales
 class Conejos(Animales.Animales):
 	"""Creamos la clase conejos y heredamos parte de su comportamiento de la clase Animales"""
-	def __Reserva(self,reserva):
+	def __reserva(self,reserva):
 		"""Esta funcion privada o encapsulada, aumenta la cantidad de alimento que el animal tiene almacenado"""
-		self.ReservaAlimenticia+=reserva
-	def Alimentarse(self,TipoAlimento,CantidadAlimento):					
+		self.reserva_alimenticia+=reserva
+	def Alimentarse(self,tipo_alimento,cantidad_alimento):					
 		"""Recibe el tipo de alimento y la cantidad del mismo que se prentende dar a los conejos, si es pasto llama a la funcion __Reserva caso contrario manda mensaje"""
-		if(TipoAlimento.lower()=="pasto"):
-			self.__Reserva(CantidadAlimento)
+		if(tipo_alimento.lower()=="pasto"):
+			self.__reserva(cantidad_alimento)
 		else:
-			print("No debe alimentar a animales herviboros con {0}".format(TipoAlimento))
+			print("No debe alimentar a animales herviboros con {0}".format(tipo_alimento))
